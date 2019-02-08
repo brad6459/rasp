ca93a461e85ec7cea9ec4b4778ff502f04f9727d
Brindan <brad6459@colorado.edu>
2019-02-07 18:15:46 -0700

    Arduino Stuff

---
+++ Arduino/Two_ultra_in_parallel/Two_in_parallel.ino
@@ -0,0 +1,55 @@
+// defines pins numbers
+const int trigPin1 = 9;
+const int echoPin1 = 10;
+const int trigPin2 = 4;
+const int echoPin2 = 5;
+
+// defines variables
+
+long duration;
+float distance;
+
+void setup() {
+pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
+pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
+
+Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+
+// #1
+// Clears the trigPin
+digitalWrite(trigPin1, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin1, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin1, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin1, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance#1: ");
+Serial.println(distance);
+
+#2
+// Clears the trigPin
+digitalWrite(trigPin2, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin2, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin2, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin2, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance#2: ");
+Serial.println(distance);
+
+
+}
