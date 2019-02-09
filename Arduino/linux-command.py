e1f32ee12af90b9cac2359b4b43fa3b22087ded5
Brindan <brad6459@colorado.edu>
2019-02-09 02:27:07 -0700

    ros command example

--- Arduino/linux-command.py
+++ Arduino/linux-command.py
@@ -1,9 +1,5 @@
 import RPi.GPIO as GPIO
-import time
-import sys
-import Tkinter as tk
-from picamera import PiCamera
-import os 
+import time 
 from time import sleep
 
 GPIO.setwarnings(False)
@@ -11,8 +7,16 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
 
-
-GPIO.output(5,GPIO.LOW)
-time.sleep(30)
-GPIO.output(5,GPIO.HIGH)
-GPIO.output(5,GPIO.LOW)
+while True:
+	GPIO.output(3,GPIO.LOW)
+	GPIO.output(5,GPIO.LOW)
+	status = input("Semi-aut (0) or Discontinuity? (1)   :  ")
+	
+	if status == 0:
+		GPIO.output(3,GPIO.HIGH)
+		time.sleep(10)
+	elif status == 1:
+		GPIO.output(5,GPIO.HIGH)
+		time.sleep(10)
+	else:
+		print("Pick 0 or 1")
